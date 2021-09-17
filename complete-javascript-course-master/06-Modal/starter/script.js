8ed2e70d64129ef1e24890f9a06e8199e30e8dc5
vinayak0127 <vinayakrajbusiness@gmail.com>
2021-09-17 23:58:57 +0530

    Project-2 modal

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsOpen = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpen.length; i++) {
+  btnsOpen[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
